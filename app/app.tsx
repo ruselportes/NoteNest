43db84069b1b38d74b0127f61668a3cd9c480ed4
aleahampo <aleahampo@gmail.com>
2025-10-17 16:11:49 +0800

    Add navigation

--- app/app.tsx
+++ app/app.tsx
@@ -1,7 +1,17 @@
-import {View,Text} from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './login/LoginScreen';
 import RegisterScreen from './register/RegisterScreen';
-export default function App(){
-    return (
-        <RegisterScreen />
-    )
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    
+  );
 }
